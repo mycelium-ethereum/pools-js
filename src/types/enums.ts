cac98dbafd1d496b16924e2ce9b918bd3c33f934
dospore <tetthera@gmail.com>
2022-02-03 11:27:04 +1000

    Add flip commit types

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -3,10 +3,12 @@
  * @enum
  */
 export enum CommitEnum {
-    shortMint = 0,
-    shortBurn = 1,
-    longMint = 2,
-    longBurn = 3,
+    shortMint = 0, // Mint short tokens
+    shortBurn = 1, // Burn short tokens
+    longMint = 2, // Mint long tokens
+    longBurn = 3, // Burn long tokens
+    longBurnShortMint = 4, // Burn Long tokens, then instantly mint in same upkeep
+    shortBurnLongMint= 5 // Burn Short tokens, then instantly mint in same upkeep
 }
 
 // side types
